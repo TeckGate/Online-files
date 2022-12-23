919cbc2f3e223fd09e40321129f633e82a4ef1fc
TeckGate <93782387+TeckGate@users.noreply.github.com>
2022-12-23 03:15:51 +0100

    Update vctotxt.js

--- vctotxt.js
+++ vctotxt.js
@@ -1,160 +1,292 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Metadata Powered by www.technicalarp.com -->
-    <title>Voice To Text Converter</title>
-    <meta name="description"content="Voice To Text Converter tool online for free in many language."/>
-    <meta name="robots" content="noodp" />
-    <meta name="keywords"content="Voice To Text Converter tool online for free in many language."/>
-    <meta property="og:locale" content="en_US" />
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content="Voice To Text Converter" />
-	<meta itemprop="url" content="yoursiteurl" />
-    <meta itemprop="name" content="Voice To Text Converter" />
-	<meta name="author" content="Technical Arp" />
-    <meta property="og:description" content="Voice To Text Converter tool online for free in many language."/>
-    <meta property="og:site_name"content="Voice To Text Converter tool online for free in many language."/>
-    <meta property="og:image"content="https://cloud.google.com/images/products/speech/speech-api-lead.png">
-    <!-- / Powered by www.technicalarp.com free version -->
-
-    <!-- Favicons-->
-	<link rel="shortcut icon" href="https://1.bp.blogspot.com/-SuTIvxc7uKw/YOFG1-Rbj9I/AAAAAAAAAdU/zJalbC4Mjog5NsOPpCtNoEVl4X0EJtFBQCLcBGAsYHQ/s320/keyphrase.png">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-<style>
-	@import url("https://fonts.googleapis.com/css?family=Open+Sans");
-
-	html {
-	font-size: 10px;
-	}
-
-	body {
-	background: #fce6d3;
-	font-family: "Open Sans", sans-serif;
-	font-size: 2rem;
-	color: #1d2e4b;
-	}
-
-	.technicalarpwords {
-	max-width: 800px;
-	margin: 50px auto;
-	background: #fff;
-	border-radius: 5px;
-	padding: 1rem 2rem 1rem 5rem;
-	background: -webkit-gradient(linear, 0 0, 0 100%, from(#cfddf9), color-stop(4%, #fff)) 0 4px;
-	background-size: 100% 3rem;
-	position: relative;
-	line-height: 3rem;
-	-moz-box-shadow: 2px 8px 15px rgba(228, 126, 118, 0.5);
-	-webkit-box-shadow: 2px 8px 15px rgba(228, 126, 118, 0.5);
-	box-shadow: 2px 8px 15px rgba(228, 126, 118, 0.5);
-	}
-
-	p {
-	margin: 0 0 3rem;
-	}
-
-	.technicalarpwords:before {
-	content: "";
-	position: absolute;
-	width: 4px;
-	top: 0;
-	left: 30px;
-	bottom: 0;
-	border: 1px solid;
-	border-color: transparent #ffd9d9;
-	}
-	{"mode":"full","isActive":false}
-
-	.adspacebytechnicalarp{ max-width:700px; padding:2em;box-shadow:0 5px 15px rgba(0,0,0,.16);border-radius:5px;margin-top:1em;background:#fff;text-align:left}
-
-</style>
-
-</head>
-<body>
-<center>
-<br>
-<img src="https://cloud.google.com/images/products/speech/speech-api-lead.png"></img> 
-
-<h3> Voice To Text Note Maker Tool </h3>
-
-</center>
-<br> 
-
-<div class="container" >
-
-				<div class="adspacebytechnicalarp container">
-					Place your Ad Code 1     
-				 </div>
-				</br>
-
-	<div id="info" style="color: red; font-size: 16px; padding: 3px 6px; text-align:center;">
-			<div id="info_start">
-			Click on the microphone icon and start speaking.
-			 </div>
-			<div id="info_speak_now" style="display: none;">
-			Speak now || बोलना शुरू करें !
-			 </div>
-			<div id="info_no_speech" style="display: none;">
-			No speech was detected. adjust your microphone settings.
-			 </div>
-			<div id="info_no_microphone" style="display: none;">
-			No microphone.! Ensure that a microphone configured correctly.
-			 </div>
-			<div id="info_allow" style="display: none;">
-			Click the "Allow" button above to enable your microphone.
-			 </div>
-			<div id="info_denied" style="display: none;">
-			Permission to use microphone was denied.
-			 </div>
-			<div id="info_blocked" style="display: none;">
-			Permission to use microphone is blocked. To change, go to chrome://settings/contentExceptions#media-stream
-			 </div>
-			<div id="info_upgrade" style="display: none;">
-			This facility is not supported by your browser. Upgrade to <a href="https://www.google.com/chrome" target="_blank">Chrome</a>.
-			 </div>
-		<br>
-	</div>
-
-	<div class="flex-box" style="text-align:center;">
-		<select id="select_language" class="" onchange="updateCountry()" style=""></select>
-		<select id="select_dialect" style="margin: 1px 0px; padding: 0px;"></select>
-		<button id="start_button" onclick="startButton(event)" style="background: #fff; padding: 0;" title="Click and Start Speaking">
-		<img alt="Start" height="30" id="start_img" src="https://www.google.com/intl/en/chrome/assets/common/images/content/mic.gif" width="54" /></button>
-	</div>
-
-	<div class="technicalarpwords" contenteditable>
-		<div id="results" class="technicalarp-converter" >
-			<span class="final" id="final_span" ></span> <span class="interim" id="interim_span" style="color: red;"></span>
-		</div>
-	</div>
-
-	<div class="technicalarp-wrapper flex-box" style="text-align:center;">
-		<input class="btn btn-lg btn-success" id="button1" onclick="CopyToClipboard('results')" value="Copy" title="Copy Text" type="button"/>
-	</div>
-
-				<div class="adspacebytechnicalarp container">
-					Place your Ad Code 2  
-				 </div>
-				</br>
-
-		<footer id='footer' style="">
-                <p class="text-center" >Copyright &#169; <script type='text/javascript'>var creditsyear = new Date();document.write(creditsyear.getFullYear());</script> 
-				<a href="yourwebsiteurl" >YOUR WEBSITE</a></p>
-				<div id="credits" class="text-center" style="margin-bottom:1.5rem;" >
-					Distributed With 💗 by <a href="https://www.technicalarp.com/" rel="ugc" id="creditlink">Technical Arp</a>
-					<!--Released under the Creative Commons Attribution-ShareAlike 3.0 Unported License. So you can use but don't allow to remove credits.-->
-					</br>
-				</div>			
-        </footer>
-		
-</div>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js" integrity="sha512-AFwxAkWdvxRd9qhYYp1qbeRZj6/iTNmJ2GFwcxsMOzwwTaRwz2a/2TX225Ebcj3whXte1WGQb38cXE5j7ZQw3g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-<script src="https://github.com/TeckGate/OnlineWebFiles/blob/main/voicetotext.js"></script>
+/*!
+ * Original Credits of version v1.0.0:
+ *
+ * NOTE: Technical Arp is not original developer of script.
+ * If your are developer this script v1.0.0 then please message/contact us for proper credits. We will be happy to give proper credits.
+ */
  
-<body>
-<html>
+/*!
+ * Updated Script!
+ * Voice to Text Note Maker v2.1.0
+ *
+ * For more information visit: https://www.technicalarp.com/
+ * NOTE: Installation and usage are posted on our website, kindly visit website and search for Voice to Text Script.
+ *
+ * Copyright (c) 2021 www.technicalarp.com
+ * Released under the Creative Commons Attribution-ShareAlike 3.0 Unported License.
+ * Use with proper credits.
+ */
+
+var langs = [
+		['Hindi',	['hi-IN']],
+		['English', ['en-AU', 'Australia'],
+					['en-CA', 'Canada'],
+					['en-IN', 'India'],
+					['en-NZ', 'New Zealand'],
+					['en-ZA', 'South Africa'],
+					['en-GB', 'United Kingdom'],
+					['en-US', 'United States']
+		]
+	];
+	
+	for (var i = 0; i < langs.length; i++) {
+	  select_language.options[i] = new Option(langs[i][0], i);
+	}
+	select_language.selectedIndex = 0;
+	updateCountry();
+	select_dialect.selectedIndex = 0;
+	showInfo('info_start');
+
+	function updateCountry() {
+	  for (var i = select_dialect.options.length - 1; i >= 0; i--) {
+		select_dialect.remove(i);
+	  }
+	  var list = langs[select_language.selectedIndex];
+	  for (var i = 1; i < list.length; i++) {
+		select_dialect.options.add(new Option(list[i][1], list[i][0]));
+	  }
+	  select_dialect.style.visibility = list[1].length == 1 ? 'hidden' : 'visible';
+	}
+
+	var create_email = false;
+	var final_transcript = '';
+	var recognizing = false;
+	var ignore_onend;
+	var start_timestamp;
+	if (!('webkitSpeechRecognition' in window)) {
+	  upgrade();
+	} else {
+	  start_button.style.display = 'inline-block';
+	  var recognition = new webkitSpeechRecognition();
+	  recognition.continuous = true;
+	  recognition.interimResults = true;
+
+	  recognition.onstart = function() {
+		recognizing = true;
+		showInfo('info_speak_now');
+		start_img.src = 'https://www.google.com/intl/en/chrome/assets/common/images/content/mic-animate.gif';
+	  };
+
+	  recognition.onerror = function(event) {
+		if (event.error == 'no-speech') {
+		  start_img.src = 'https://www.google.com/intl/en/chrome/assets/common/images/content/mic.gif';
+		  showInfo('info_no_speech');
+		  ignore_onend = true;
+		}
+		if (event.error == 'audio-capture') {
+		  start_img.src = 'https://www.google.com/intl/en/chrome/assets/common/images/content/mic.gif';
+		  showInfo('info_no_microphone');
+		  ignore_onend = true;
+		}
+		if (event.error == 'not-allowed') {
+		  if (event.timeStamp - start_timestamp < 100) {
+			showInfo('info_blocked');
+		  } else {
+			showInfo('info_denied');
+		  }
+		  ignore_onend = true;
+		}
+	  };
+
+	  recognition.onend = function() {
+		recognizing = false;
+		if (ignore_onend) {
+		  return;
+		}
+		start_img.src = 'https://www.google.com/intl/en/chrome/assets/common/images/content/mic.gif';
+		if (!final_transcript) {
+		  showInfo('info_start');
+		  return;
+		}
+		showInfo('');
+		if (window.getSelection) {
+		  window.getSelection().removeAllRanges();
+		  var range = document.createRange();
+		  range.selectNode(document.getElementById('final_span'));
+		  window.getSelection().addRange(range);
+		}
+		if (create_email) {
+		  create_email = false;
+		  createEmail();
+		}
+	  };
+
+	  recognition.onresult = function(event) {
+		var interim_transcript = '';
+		if (typeof(event.results) == 'undefined') {
+		  recognition.onend = null;
+		  recognition.stop();
+		  upgrade();
+		  return;
+		}
+		for (var i = event.resultIndex; i < event.results.length; ++i) {
+		  if (event.results[i].isFinal) {
+			final_transcript += event.results[i][0].transcript;
+		  } else {
+			interim_transcript += event.results[i][0].transcript;
+		  }
+		}
+		final_transcript = capitalize(final_transcript);
+		final_span.innerHTML = linebreak(final_transcript);
+		interim_span.innerHTML = linebreak(interim_transcript);
+		if (final_transcript || interim_transcript) {
+		  showButtons('inline-block');
+		}
+	  };
+
+	  recognition.onresult = function(event) {
+		var interim_transcript = '';
+		if (typeof(event.results) == 'undefined') {
+		  recognition.onend = null;
+		  recognition.stop();
+		  upgrade();
+		  return;
+		}
+		for (var i = event.resultIndex; i < event.results.length; ++i) {
+		  if (event.results[i].isFinal) {
+			final_transcript += event.results[i][0].transcript;
+		  } else {
+			interim_transcript += event.results[i][0].transcript;
+		  }
+		}
+		final_transcript = capitalize(final_transcript);
+		final_span.innerHTML = linebreak(final_transcript);
+		interim_span.innerHTML = linebreak(interim_transcript);
+		if (final_transcript || interim_transcript) {
+		  showButtons('inline-block');
+		}
+	  };
+	  
+	}
+
+	function upgrade() {
+	  start_button.style.visibility = 'hidden';
+	  showInfo('info_upgrade');
+	}
+
+	var two_line = /\n\n/g;
+	var one_line = /\n/g;
+	function linebreak(s) {
+	  return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
+	}
+
+	var first_char = /\S/;
+	function capitalize(s) {
+	  return s.replace(first_char, function(m) { return m.toUpperCase(); });
+	}
+
+	function createEmail() {
+	  var n = final_transcript.indexOf('\n');
+	  if (n < 0 || n >= 80) {
+		n = 40 + final_transcript.substring(40).indexOf(' ');
+	  }
+	  var subject = encodeURI(final_transcript.substring(0, n));
+	  var body = encodeURI(final_transcript.substring(n + 1));
+	  window.location.href = 'mailto:?subject=' + subject + '&body=' + body;
+	}
+
+	function copyButton() {
+	  if (recognizing) {
+		recognizing = false;
+		recognition.stop();
+	  }
+	  copy_button.style.display = 'none';
+	  copy_info.style.display = 'inline-block';
+	  showInfo('');
+	}
+
+	function emailButton() {
+	  if (recognizing) {
+		create_email = true;
+		recognizing = false;
+		recognition.stop();
+	  } else {
+		createEmail();
+	  }
+	  email_button.style.display = 'none';
+	  email_info.style.display = 'inline-block';
+	  showInfo('');
+	}
+
+	function startButton(event) {
+	  if (recognizing) {
+		recognition.stop();
+		return;
+	  }
+	  final_transcript = '';
+	  recognition.lang = select_dialect.value;
+	  recognition.start();
+	  ignore_onend = false;
+	  final_span.innerHTML = '';
+	  interim_span.innerHTML = '';
+	  start_img.src = 'https://www.google.com/intl/en/chrome/assets/common/images/content/mic-slash.gif';
+	  showInfo('info_allow');
+	  showButtons('none');
+	  start_timestamp = event.timeStamp;
+	}
+
+	function showInfo(s) {
+	  if (s) {
+		for (var child = info.firstChild; child; child = child.nextSibling) {
+		  if (child.style) {
+			child.style.display = child.id == s ? 'inline' : 'none';
+		  }
+		}
+		info.style.visibility = 'visible';
+	  } else {
+		info.style.visibility = 'hidden';
+	  }
+	}
+
+	var current_style;
+	function showButtons(style) {
+	  if (style == current_style) {
+		return;
+	  }
+	  current_style = style;
+	  copy_button.style.display = style;
+	  email_button.style.display = style;
+	  copy_info.style.display = 'none';
+	  email_info.style.display = 'none';
+	}
+
+	function saveTextAsUnicode() {
+    var textToSave = document.getElementById("results").value;
+    var textToSaveAsBlob = new Blob([textToSave], {type:"text/Doc"});
+	//var textToSaveAsBlob = new Blob([textToSave], {type:"text/plain"});
+    var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
+	//var fileNameToSaveAs = document.getElementById("inputFileNameToSaveAs").value;
+    var fileNameToSaveAs = "Text in Unicode.doc";
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    downloadLink.href = textToSaveAsURL;
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+}
+
+function destroyClickedElement(event) {
+    document.body.removeChild(event.target);
+}
+
+function CopyToClipboard(results) {
+if (document.selection) { 
+    var range = document.body.createTextRange();
+    range.moveToElementText(document.getElementById(results));
+    range.select().createTextRange();
+    document.execCommand("copy"); 
+
+} else if (window.getSelection) {
+    var range = document.createRange();
+     range.selectNode(document.getElementById(results));
+     window.getSelection().addRange(range);
+     document.execCommand("copy");
+//     alert("Text Copied,") 
+}
+
+     btn = document.getElementById("button1");
+     btn.value = 'Selected Text Copied!';
+	 setInterval(function(){ btn.value = 'Copy!'; }, 2000);
+}
